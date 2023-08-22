810ecf823dfb8b38f5425cc1b2ea774b00d345c3
noelfoka <nomgnenoel@gmail.com>
2023-08-22 06:59:57 +0100

    update

--- starter/src/index.js
+++ starter/src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+//import store and Provider
+import { Provider } from "react-redux";
+import { store } from "./store";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
